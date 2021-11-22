041e16c018719aedb82ddc20a72ea31994d0f2d1
Riey <creeper844@gmail.com>
2021-11-22 16:49:32 +0900

    Add profiling font texture

--- src/render/cell.rs
+++ src/render/cell.rs
@@ -47,6 +47,8 @@ impl CellContext {
         font_size: f32,
         scale_factor: f32,
     ) -> Self {
+        profiling::scope!("Create CellContext");
+
         let font_size = font_size * scale_factor;
 
         let font = FontRef::from_index(super::FONT, 0).unwrap();
@@ -243,57 +245,66 @@ impl CellContext {
 
         let mut scaler = scale_ctx.builder(font).hint(true).size(font_size).build();
 
-        font.charmap().enumerate(|_c, id| {
-            image.clear();
-            if Render::new(&[
-                Source::ColorBitmap(StrikeWith::BestFit),
-                Source::ColorOutline(0),
-                Source::Bitmap(StrikeWith::BestFit),
-                Source::Outline,
-            ])
-            .render_into(&mut scaler, id, &mut image)
-            {
-                if image.placement.width == 0 || image.placement.height == 0 {
-                } else {
-                    let alloc = allocator.alloc(image.placement.width, image.placement.height);
-                    if let Some(new_page) = alloc.layer.checked_sub(layer_count) {
-                        data.extend(std::iter::repeat(0).take(TEXTURE_SIZE * new_page as usize));
-                        layer_count += new_page;
-                    }
-                    let page = &mut data[TEXTURE_SIZE * alloc.layer as usize..][..TEXTURE_SIZE];
-                    let left_top = (alloc.y * TEXTURE_WIDTH + alloc.x) as usize;
+        {
+            profiling::scope!("Create font texture");
 
-                    for (row_index, row) in image
-                        .data
-                        .chunks_exact(image.placement.width as usize)
-                        .enumerate()
-                    {
-                        let begin = left_top + row_index * TEXTURE_WIDTH as usize;
-                        let end = begin + row.len();
-                        page[begin..end].copy_from_slice(row);
+            font.charmap().enumerate(|_c, id| {
+                image.clear();
+                if Render::new(&[
+                    Source::ColorBitmap(StrikeWith::BestFit),
+                    Source::ColorOutline(0),
+                    Source::Bitmap(StrikeWith::BestFit),
+                    Source::Outline,
+                ])
+                .render_into(&mut scaler, id, &mut image)
+                {
+                    if image.placement.width == 0 || image.placement.height == 0 {
+                    } else {
+                        let alloc = allocator.alloc(image.placement.width, image.placement.height);
+                        if let Some(new_page) = alloc.layer.checked_sub(layer_count) {
+                            data.extend(
+                                std::iter::repeat(0).take(TEXTURE_SIZE * new_page as usize),
+                            );
+                            layer_count += new_page;
+                        }
+                        let page = &mut data[TEXTURE_SIZE * alloc.layer as usize..][..TEXTURE_SIZE];
+                        let left_top = (alloc.y * TEXTURE_WIDTH + alloc.x) as usize;
+
+                        for (row_index, row) in image
+                            .data
+                            .chunks_exact(image.placement.width as usize)
+                            .enumerate()
+                        {
+                            let begin = left_top + row_index * TEXTURE_WIDTH as usize;
+                            let end = begin + row.len();
+                            page[begin..end].copy_from_slice(row);
+                        }
+                        glyph_cache.insert(
+                            id,
+                            GlyphInfo {
+                                tex_position: [alloc.x as _, alloc.y as _],
+                                tex_size: [image.placement.width as _, image.placement.height as _],
+                                glyph_position: [
+                                    image.placement.left as _,
+                                    image.placement.top as _,
+                                ],
+                                layer: alloc.layer as _,
+                            },
+                        );
                     }
-                    glyph_cache.insert(
-                        id,
-                        GlyphInfo {
-                            tex_position: [alloc.x as _, alloc.y as _],
-                            tex_size: [image.placement.width as _, image.placement.height as _],
-                            glyph_position: [image.placement.left as _, image.placement.top as _],
-                            layer: alloc.layer as _,
-                        },
-                    );
                 }
-            }
-        });
+            });
+        }
 
-        use std::io::Write;
-        let mut out = std::fs::OpenOptions::new()
-            .write(true)
-            .create(true)
-            .open("foo.pgm")
-            .unwrap();
-        write!(out, "P5\n{} {}\n255\n", TEXTURE_WIDTH, TEXTURE_WIDTH).unwrap();
-        out.write_all(&data[..TEXTURE_SIZE]).unwrap();
-        out.flush().unwrap();
+        // use std::io::Write;
+        // let mut out = std::fs::OpenOptions::new()
+        //     .write(true)
+        //     .create(true)
+        //     .open("foo.pgm")
+        //     .unwrap();
+        // write!(out, "P5\n{} {}\n255\n", TEXTURE_WIDTH, TEXTURE_WIDTH).unwrap();
+        // out.write_all(&data[..TEXTURE_SIZE]).unwrap();
+        // out.flush().unwrap();
 
         let texture_size = wgpu::Extent3d {
             width: TEXTURE_WIDTH,
